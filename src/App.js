b6587c8a26be828fd3d648a61fa6274ab2fd8dae
Jae Yoon <jaeyoon@macbook-air.myfiosgateway.com>
2022-07-17 20:55:51 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Main from './components/MainComponent'
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {HashRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Header from './components/HeaderComponent'
 import Footer from './components/FooterComponent'
 import About from './components/AboutComponent'
